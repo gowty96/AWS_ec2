09b1878c5ff7f0384b5678c91adfeb0ae0e43eed
Ubuntu <ubuntu@ip-172-31-85-240.ec2.internal>
2022-03-19 05:14:40 +0000

    adding new scripts

---
+++ 1to10digit.sh
@@ -0,0 +1,8 @@
+echo "Enter a number"
+read num
+if [ $num -lt 10 ]
+then
+echo "one digit number printed"
+else
+echo "two digit number printed"
+fi
---
+++ while.sh
@@ -0,0 +1,7 @@
+i=0
+while [ $i -le 40 ]
+do
+echo gowtham: $i
+sleep 0.5
+((i++))
+done
